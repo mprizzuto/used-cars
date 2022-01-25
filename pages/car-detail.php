e795fee5777ce0adca43cea0f4cc1a927fcb9a78
Marco R <mprizzuto88@gmail.com>
2022-01-25 16:48:48 -0500

    create test content for car details page

---
+++ pages/car-detail.php
@@ -0,0 +1,8 @@
+<section class="car-detail">
+	<inner-column>
+		<h1>car detail</h1>
+		<p>dtauls go here</p>
+
+		<p><a href="?page=add-a-car">got a car for sale? submit it here</a></p>
+	</inner-column>
+</section>
