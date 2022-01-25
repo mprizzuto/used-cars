5e34d6a78c9d38d8c932a3dc62cf82f6530b141b
Marco R <mprizzuto88@gmail.com>
2022-01-25 16:48:06 -0500

    create router for pages

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-index.php
+<?php 
+$page = $_GET["page"] ?? null;
+
+//get the header
+require "components/header.php";
+// var_dump($_GET);
+?>
+
+<main class="site-main">
+	<!-- page router for main page content -->
+<?php 
+switch ($page) {
+	case "cars":
+		include "pages/cars.php";
+		break;
+
+		case "car-detail":
+		include "pages/car-detail.php";
+		break;
+
+		case "add-a-car":
+		include "pages/car-form.php";
+		break;
+	
+	default:
+		include "pages/default.php";
+}
+?>
+</main>
+
+
+<?php 
+require "components/footer.php";
+?>
