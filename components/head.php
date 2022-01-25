00f40cadd7aa905055b43e42bc64d40093922803
Marco R <mprizzuto88@gmail.com>
2022-01-25 16:44:52 -0500

    generate dynamic metadata

--- components/head.php
+++ components/head.php
@@ -1,1 +1,17 @@
-head.php
+<?php require_once "./functions.php";?>
+
+<?php 
+switch($page) {
+	case "cars";
+	createHead("cars page", "this is the cars page");
+	break;
+
+	case "car-detail";
+	createHead("car details", "the car detail page");
+	break;
+
+	default: 
+	createHead("homepage", "this is the homepage");
+}
+
+ ?>
