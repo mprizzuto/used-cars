b3533d8b84282c4065e8c943a9cd05d0c47cd9e1
Marco R <mprizzuto88@gmail.com>
2022-01-25 14:36:28 -0500

    create database folder and databased

---
+++ database/meta.php
@@ -0,0 +1,21 @@
+<?php 
+include "./functions.php";
+$metaData = [
+	[
+		"title" => "used car home",
+		"description" => "Uncle Ed's used car search",
+	],
+	[
+		"title" => "used cars",
+		"description" => "find all the used cars for sale here",
+	],
+	[
+		"title" => "used cars details page",
+		"description" => "all the used car details are here!",
+	],
+];
+/*
+"title" => "",
+"description" => "",
+*/ 
+?>
