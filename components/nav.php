c0481329717f44fbb4033c384372571baa347904
Marco R <mprizzuto88@gmail.com>
2022-01-25 16:45:30 -0500

    create nav item data structure, generate links

--- components/nav.php
+++ components/nav.php
@@ -1,1 +1,14 @@
-nav.php
+<?php 
+$navItems = [
+	"home" => "index.php",
+	"cars" => "?page=cars",
+	"car-detail" => "?page=car-detail",
+
+];
+?>
+
+<nav>
+<?php foreach ($navItems as $key => $value) { ?>
+	<a href="<?=$value?>"><?=$key?></a>
+<?php } ?>
+</nav>
