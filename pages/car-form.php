01f6bbfa82ef8fe6607db0f6180e63da0d044d92
Marco R <mprizzuto88@gmail.com>
2022-02-03 23:19:03 -0500

    create form labels

--- pages/car-form.php
+++ pages/car-form.php
@@ -2,6 +2,20 @@
 	<inner-column>
 		<h1>car form</h1>
 
-		<p><mark>coming soon</mark> submit your own car for sale!</p>
+	<!-- 	<p><mark>coming soon</mark> submit your own car for sale!</p> -->
+
+	<form class="submit-used-car">
+		<label for="model">
+			<input type="text" name="model" id="model">
+		</label>
+
+		<label for="price">
+			<input type="text" name="price" id="price">
+		</label>
+
+		<label for="mileage">
+			<input type="mileage" name="mileage" id="mileage">
+		</label>
+	</form>
 	</inner-column>
 </section>
