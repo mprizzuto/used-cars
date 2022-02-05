1aed242d94e9a65a9459f4e96037d0de9f60df7f
Marco R <mprizzuto88@gmail.com>
2022-02-04 23:24:47 -0500

    generate used cars using usedCarGenerator function

--- pages/cars.php
+++ pages/cars.php
@@ -1,5 +1,10 @@
 <section class="cars">
 	<inner-column>
 		<h1>cars</h1>
+		<?php 
+		usedCarGenerator("jeep", 2011, "red", ["./images/cars/car-one.jpg", "./images/cars/car-two.jpg"], "rick, flag and eagle");
+
+		usedCarGenerator("nissan", 2018, "green", ["./images/cars/car-one.jpg", "./images/cars/car-two.jpg"], "ride on");
+		?>
 	</inner-column>
 </section>
